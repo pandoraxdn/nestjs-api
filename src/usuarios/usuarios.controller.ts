30d42aef20d5e5304afa5e9d331500464317fb6c
pandoraxdn <51171407+pandoraxdn@users.noreply.github.com>
2024-02-26 22:39:51 -0600

    Update usuarios.controller.ts

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -12,12 +12,10 @@ import { UsuariosService } from './usuarios.service';
 import { CrearUsuarios } from './dto/crear-usuarios.dto';
 import { ActualizarUsuarios } from './dto/actualizar-usuarios.dto';
 
-
 @Controller('usuarios')
 export class UsuariosController {
 
     constructor( private usuariosService: UsuariosService ){}
-
 
     @Post()
     async create( @Body( new ValidationPipe() ) createUser: CrearUsuarios ){
@@ -28,21 +26,20 @@ export class UsuariosController {
     async update( @Param('id_usuario') id_usuario: number, @Body( new ValidationPipe() ) updateUser: ActualizarUsuarios ){
         return await this.usuariosService.update( id_usuario, updateUser );
     }
-    
 
     @Get()
     async findAll(){
-        return this.usuariosService.findAll();
+        return await this.usuariosService.findAll();
     }
 
     @Get(':id_usuario')
     async findOne( @Param('id_usuario') id_usuario: number ){
-        return this.usuariosService.findOne( id_usuario );
+        return await this.usuariosService.findOne( id_usuario );
     }
 
     @Delete(':id_usuario')
     async delete( @Param('id_usuario') id_usuario: number ){
-        return this.usuariosService.delete( id_usuario );
+        return await this.usuariosService.delete( id_usuario );
     }
 
 }
