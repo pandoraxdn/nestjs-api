0d17c0b3e669b6922a8275278306172b54e5cab4
pandoraxdn <51171407+pandoraxdn@users.noreply.github.com>
2024-02-26 22:37:55 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-
-
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
+  
   await app.listen(3000);
+  
 }
+
 bootstrap();
